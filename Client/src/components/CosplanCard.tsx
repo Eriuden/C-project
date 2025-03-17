c9c9fdbdcbfcb197e3c308bb4617b1b0b268f0b5
Eriuden <antoinecarrette@hotmail.fr>
2025-03-17 10:06:14 +0100

    CosplanCard 1 17/03/2025

--- Client/src/components/CosplanCard.tsx
+++ Client/src/components/CosplanCard.tsx
@@ -1,8 +1,19 @@
+//Il faut un button pour afficher la page du cosplan avec les cards des parts
 
-const CosplanCard = ({cosplanProps}) => {
+import { Link } from "react-router-dom"
+
+type cosplanPropsType = {
+  _id: string,
+  picture: string,
+  name: string,
+  licence: string,
+  budget: string,
+  requiredMaterials: string
+}
+const CosplanCard = (cosplanProps :cosplanPropsType) => {
   return (
     <div>
-      
+      <Link className='w-[100%] text-center m-auto text-black' to={"/cosplan"}>Vos conseils</Link>
     </div>
   )
 }
