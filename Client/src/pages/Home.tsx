d0601b219dd30450bb6d19c9b544dee84493bcc8
Eriuden <antoinecarrette@hotmail.fr>
2025-03-17 10:20:06 +0100

    Home 2 17/03/2025

--- Client/src/pages/Home.tsx
+++ Client/src/pages/Home.tsx
@@ -1,26 +1,37 @@
 import CosplanCard from "../components/CosplanCard"
 import { isEmpty } from "../utils"
 import { useSelector } from "react-redux";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { UidContext } from "../AppContext";
 
 const Home = () => {
 
   const [isChecked, setisChecked] = useState(false)
   const cosplans = useSelector((state:any) => state.cosplanReducer);
 
+  const uid = useContext(UidContext)
+
   const setIsCheckedOrNot = () =>
   setisChecked(!isChecked)
-  
+
   return (
-    <div>
-      {!isEmpty(cosplans[0]) &&
-      cosplans.map((cosplan:any) => {
-        <CosplanCard cosplanProps={cosplan}/>
-        {!isChecked  ?<button className="bg-green" onClick={setIsCheckedOrNot}>V</button>
-        : <button className="bg-red">X</button>}
-      })}
-      
-    </div>
+    <>
+      {uid ? (
+        <>
+          <div>
+            {!isEmpty(cosplans[0]) &&
+            cosplans.map((cosplan:any) => {
+              <CosplanCard cosplanProps={cosplan}/>
+              {!isChecked ? <button className="bg-green" onClick={setIsCheckedOrNot}>V</button>
+              : <button className="bg-red">X</button>}
+            })}       
+          </div> 
+        </>    
+      )
+        : ""
+      }  
+    </>
+     
   )
 }
 
