1487944d1c9ac43b0ca4b85ac174f0fa746b745c
Eriuden <antoinecarrette@hotmail.fr>
2025-03-19 08:28:02 +0100

    build : Cosplan part card 1 19/03/2025

--- Client/src/components/cosplanPartCard.tsx
+++ Client/src/components/cosplanPartCard.tsx
@@ -1,7 +1,32 @@
-export const cosplanPartCard = () => {
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
+import { addCosplanPart, getCosplans } from "../redux/actions/cosplan.action";
+
+
+export const cosplanPartCard = (cosplan: any) => {
+
+  type appDispatch = () => any
+
+  const [instruction, setInstruction] = useState("");
+  const userData = useSelector((state:any) => state.userReducer);
+  const useAppDispatch = () => useDispatch<appDispatch>()
+  const dispatch = useAppDispatch()
+
+  const handlePart = (e:any) => {
+    e.prevenDefault();
+
+    if (instruction) {
+      addCosplanPart(cosplan._id, userData, dispatch)
+        .then(() => getCosplans(cosplan, dispatch))
+        .then(() => setInstruction(""));
+    }
+  };
+
   return (
     <div>
-      
+      {cosplan.parts.map((part:any)=> {
+        
+      })}
     </div>
   )
 }
