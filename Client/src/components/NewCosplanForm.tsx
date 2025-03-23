986773ab86f23d7c161d180ebf40cd9bed70f174
Eriuden <antoinecarrette@hotmail.fr>
2025-03-23 08:37:03 +0100

    build : new cosplan form 1 23/03/2025

--- Client/src/components/NewCosplanForm.tsx
+++ Client/src/components/NewCosplanForm.tsx
@@ -1,4 +1,21 @@
+import { useSelector, useDispatch } from "react-redux"
+import { useState } from "react"
 export const NewCosplanForm = () => {
+
+const [picture, setPicture] = useState("")
+const [name, setName] = useState("")
+const [licence, setLicence] = useState("")
+const [budget, setBudget] = useState("")
+const [requiredMaterials, setRequiredMaterials] = useState([""])
+
+const userData= useSelector((state:any) => state.userReducer)
+const errors = useSelector((state:any) => state.errorReducer.postError)
+const dispatch = useDispatch()
+
+const handlePicture = (e:any) => {
+    setPicture(URL.createObjectURL(e.target.files[0]))
+    }
+
   return (
     <div>
       
