c82a28db84e7ef4cc020cbaea10b724224be2e14
Eriuden <antoinecarrette@hotmail.fr>
2025-03-25 13:27:04 +0100

    build : part component 2 25/03/2025

--- Client/src/components/CosplanPart.tsx
+++ Client/src/components/CosplanPart.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
-import { useDispatch, useSelector, UseSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { addCosplanPart, getCosplans } from "../redux/actions/cosplan.action"
-import { isEmpty } from "../utils"
+import { EditDeleteCosplanPart } from "./EditDeleteCosplanPart"
 
 export const CosplanPart = (cosplan: any) => {
 
@@ -12,6 +12,7 @@ export const CosplanPart = (cosplan: any) => {
   const [instructions, setInstructions] = useState("")
 
   const cosplanData = useSelector((state:any) => state.cosplanReducer)
+  const userData = useSelector((state:any) => state.userReducer)
   const useAppDispatch = () => useDispatch<appDispatch>()
   const dispatch = useAppDispatch
 
@@ -24,10 +25,58 @@ export const CosplanPart = (cosplan: any) => {
         .then(() => setpartName(""))
     }
   }
-
+  //Modifier la partie materials, cas encore jamais traité ici, du à l'array
   return (
     <div>
-      
+      {cosplan.parts.map((part:any) => {
+        return (
+          <div>
+            <img src={part.partPicture}/>
+            <h2>{part.partName}</h2>
+            <ul>
+              {cosplan.parts.materials.map((material:any) => {
+                <li>{material}</li>
+              })}
+            </ul>
+            <p>{cosplan.instruction}</p>
+            <EditDeleteCosplanPart/>
+          </div>
+        )
+      })}
+
+      {
+        userData._id && (
+          <form action="" onSubmit={handlePart} className="comment-form">
+            <input
+              type="text"
+              name="partName"
+              onChange={(e) => setpartName(e.target.value)}
+              value={partName}
+              placeholder="Nom de la partie du cosplay"
+            />
+            <br />
+
+            <input
+              type="text"
+              name="materials"
+              onChange={(e) => setMaterials(e.target.value)}
+              value={materials}
+              placeholder="Nom de la partie du cosplay"
+            />
+            <br />
+
+            <input
+              type="text"
+              name="instructions"
+              onChange={(e) => setInstructions(e.target.value)}
+              value={instructions}
+              placeholder="Nom de la partie du cosplay"
+            />
+            <br />
+            <input type="submit" value="Envoyer" />
+          </form>
+        )
+      }
     </div>
   )
 }
