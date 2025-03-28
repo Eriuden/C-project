5c9036b183ff5f924a272673df2eff3e5098ee87
Eriuden <antoinecarrette@hotmail.fr>
2025-03-28 08:21:40 +0100

    build : appContext & EditDeleteCosplanPart 1 28/03/2025

--- Client/src/components/EditDeleteCosplanPart.tsx
+++ Client/src/components/EditDeleteCosplanPart.tsx
@@ -1,4 +1,18 @@
-export const EditDeleteCosplanPart = () => {
+import { useState, useEffect,useContext } from "react"
+import { deleteCosplanPart, editCosplanPart } from "../redux/actions/cosplan.action"
+
+type edelprops = {
+  _id: string,
+  cosplanId: string,
+  partName: string,
+  materials: [string],
+  instructions: string
+}
+
+export const EditDeleteCosplanPart = (edelprops: edelprops) => {
+  type appdispatch = () => any
+
+  const [isOwner, setIsOwner] = useState(false)
   return (
     <div>EditDeleteCosplanPart</div>
   )
