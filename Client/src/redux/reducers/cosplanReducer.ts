e8e3342a442e6e60f8ce66835268e549a57a99e3
Eriuden <antoinecarrette@hotmail.fr>
2025-04-20 10:10:36 +0200

    build: cosplan reducer 1 20/04/2025

--- Client/src/redux/reducers/cosplanReducer.ts
+++ Client/src/redux/reducers/cosplanReducer.ts
@@ -1,8 +1,28 @@
+import { GET_COSPLAN, UPDATE_COSPLAN, DELETE_COSPLAN } from "../actions/cosplan.action"
 
 const initialState:any = {}
 
 export const cosplanReducer = (state = initialState, action: any) => {
   switch (action.type) {
+
+    case GET_COSPLAN :
+      return action.payload 
+    
+      case UPDATE_COSPLAN:
+        return state.map((cosplan:any) => {
+           if (cosplan.id === action.payload.cosplanId) {
+               return {
+                   ...cosplan,
+                   name: action.payload.name,
+                   licence: action.payload.licence,
+                   budget: action.paylaod.budget
+               }
+           } else return cosplan
+        })
+        
+      case DELETE_COSPLAN:
+        return state.filter((cosplan:any) => cosplan._id !== action.payload.cosplanId)
+
     default:
         return state
   }
